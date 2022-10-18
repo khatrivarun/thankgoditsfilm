cf19152f5bcc49284992dc0a953ab6ce815ce992
krishaashahh <krisha.shah00@gmail.com>
2022-10-18 12:37:12 +0530

    added nav-bar and bg

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
+import NavBar from "./components/Home/NavBar";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home/NavBar";
+import Layout from "./components/Layout";
+
 const App = () => {
   return (
-    <div className='App'>
-      <h1>Thank God Its Film</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />}></Route>
+      </Route>
+    </Routes>
   );
 };
 
 export default App;
-
---
+++ client/src/components/Home/Logo/index.js
@@ -0,0 +1,16 @@
+import "./index.css";
+
+const Logo = () => {
+  return (
+    <div className="logo">
+      <h2 className="brand-title">
+        t.g.i.<h2>f</h2>
+      </h2>
+      <p className="brand-subtitle">
+        Thank God It's <p>Films</p>
+      </p>
+    </div>
+  );
+};
+
+export default Logo;
