8831b4b66f246568e51cb3a2089869d184eab734
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-10-19 09:28:29 +0530

    Folder Structure change

---
+++ src/components/Layout/index.js
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router-dom";
+import NavBar from "../NavBar";
+import "./index.css";
+
+const Layout = () => {
+  return (
+    <div className="App">
+      <NavBar />
+      <div className="page">
+        <Outlet />
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
---
+++ src/components/NavBar/index.js
@@ -0,0 +1,26 @@
+import "./index.css";
+import Logo from "../Home/Logo";
+import { Link } from "react-router-dom";
+
+const NavBar = () => (
+  <div className="nav-bar">
+    <div className="logo">
+      <Link to="/">
+        <Logo />
+      </Link>
+    </div>
+    <div className="nav-routes">
+      <Link to="about">
+        <h4>What We Do</h4>
+      </Link>
+      <Link to="team">
+        <h4>Team</h4>
+      </Link>
+      <Link to="analyze">
+        <h4>Analyze</h4>
+      </Link>
+    </div>
+  </div>
+);
+
+export default NavBar;
