ebffe13394d83dfc76801b459a1e5f720968dd29
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-10-20 12:26:34 +0530

    Generate JSON

---
+++ create-json.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+const service_account = process.env._SERVICE_ACCOUNT_KEY;
+const service_account_json = JSON.stringify(service_account);
+
+fs.writeFile('key.json', service_account_json, (error) => {
+  if (error) throw error;
+
+  console.log('Written JSON File', Object.keys(service_account_json));
+});
+
