d894e7e26e1f3523118efe6802b542affc1f02f2
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-10-20 12:36:02 +0530

    Update create-json.js

--- create-json.js
+++ create-json.js
@@ -4,9 +4,11 @@ require('dotenv').config();
 const service_account = process.env.SERVICE_ACCOUNT_KEY;
 const service_account_json = JSON.stringify(service_account);
 
+console.log(service_account_json);
+
 fs.writeFile('key.json', service_account_json, (error) => {
   if (error) throw error;
 
-  console.log('Written JSON File', Object.keys(service_account_json));
+  console.log('Written JSON File');
 });
 
